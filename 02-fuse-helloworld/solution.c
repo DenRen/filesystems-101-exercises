4d4a443aaa57f900b4fea26837fcb159dff1ceae
DenRen <denrenruslan@gmail.com>
2022-10-14 18:40:21 +0300

    Add pid to hello content

--- 02-fuse-helloworld/solution.c
+++ 02-fuse-helloworld/solution.c
@@ -3,13 +3,14 @@
 #include <fuse.h>
 #include <string.h>
 #include <errno.h>
+#include <stdio.h>
 
 const char g_file_name[] = "hello";
-const char g_file_content[] = "hello, world!\n";
+const char g_file_content[] = "hello, %d\n";
 
 static void* my_fs_init(struct fuse_conn_info* conn, struct fuse_config* cfg)
 {
-	(void) conn;
+	(void)conn;
 	cfg->kernel_cache = 1;
 
 	cfg->set_uid = 1;
@@ -23,7 +24,7 @@ static void* my_fs_init(struct fuse_conn_info* conn, struct fuse_config* cfg)
 
 static int my_fs_getattr(const char* path, struct stat* stbuf, struct fuse_file_info* file_info)
 {
-	(void) file_info;
+	(void)file_info;
 
 	memset(stbuf, 0, sizeof(*stbuf));
 
@@ -37,13 +38,17 @@ static int my_fs_getattr(const char* path, struct stat* stbuf, struct fuse_file_
 	{
 		stbuf->st_mode = S_IFREG | 0666;
 		stbuf->st_nlink = 1;
-		stbuf->st_size = strlen(g_file_content);
+
+		char buf[512] = {};
+		struct fuse_context* ctx = fuse_get_context();
+		snprintf(buf, sizeof(buf), g_file_content, ctx->pid);
+		stbuf->st_size = strlen(buf);
 	}
 	else
 	{
 		res = -ENOENT;
 	}
-	
+
 	return res;
 }
 
@@ -64,22 +69,26 @@ static int my_fs_open(const char* path, struct fuse_file_info* file_info)
 
 static int my_fs_read(const char* path, char* buf, size_t size, off_t off, struct fuse_file_info* file_info)
 {
-	(void) file_info;
+	(void)file_info;
 
 	if (strcmp(path + 1, g_file_name) != 0)
 	{
 		return -ENOENT;
 	}
 
-	const ssize_t len = strlen(g_file_content);
-	if (off < len)
+	char tmp_buf[512] = { 0 };
+	struct fuse_context* ctx = fuse_get_context();
+	snprintf(tmp_buf, sizeof(tmp_buf), g_file_content, ctx->pid);
+	const size_t len = strlen(tmp_buf);
+
+	if ((size_t)off < len)
 	{
 		if (off + size > len)
 		{
 			size = len - off;
 		}
 
-		memcpy(buf, g_file_content + off, size);
+		memcpy(buf, tmp_buf + off, size);
 	}
 	else
 	{
@@ -90,11 +99,11 @@ static int my_fs_read(const char* path, char* buf, size_t size, off_t off, struc
 }
 
 static int my_fs_readdir(const char* path, void* buf, fuse_fill_dir_t filler,
-						 off_t off, struct fuse_file_info* file_info, enum fuse_readdir_flags flags)
+	off_t off, struct fuse_file_info* file_info, enum fuse_readdir_flags flags)
 {
-	(void) off;
-	(void) file_info;
-	(void) flags;
+	(void)off;
+	(void)file_info;
+	(void)flags;
 
 	if (strcmp(path, "/") != 0)
 	{
@@ -109,20 +118,20 @@ static int my_fs_readdir(const char* path, void* buf, fuse_fill_dir_t filler,
 }
 
 static const struct fuse_operations hellofs_ops = {
-	  .init 	= my_fs_init
-	, .getattr 	= my_fs_getattr
-	, .open 	= my_fs_open
-	, .read 	= my_fs_read
-	, .readdir	= my_fs_readdir
+	  .init = my_fs_init
+	, .getattr = my_fs_getattr
+	, .open = my_fs_open
+	, .read = my_fs_read
+	, .readdir = my_fs_readdir
 };
 
-int helloworld(const char *mntp)
+int helloworld(const char* mntp)
 {
 #if 0
-	char *argv[] = {"exercise", "-f", (char *)mntp, NULL};
+	char* argv[] = { "exercise", "-f", (char*)mntp, NULL };
 	reurn fuse_main(3, argv, &hellofs_ops, NULL);
 #else
-	char *argv[] = {"./a.out", (char *)mntp, NULL};
+	char* argv[] = { "./a.out", (char*)mntp, NULL };
 	return fuse_main(2, argv, &hellofs_ops, NULL);
 #endif
 }
