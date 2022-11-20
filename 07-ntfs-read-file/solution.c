a087f4b4bf47eef997a335b372cd376556eac147
DenRen <denrenruslan@gmail.com>
2022-11-20 17:34:01 +0300

    [08]: Fix errno bug

--- 07-ntfs-read-file/solution.c
+++ 07-ntfs-read-file/solution.c
@@ -38,12 +38,13 @@ int dump_file(int img, const char *path, int out)
 	// 	* I implemented a bit hacky solution
 	//	* I use goto only for exit with error
 	//	  and don't forget to free the memory
+
 	struct ntfs_device_operations ops = {};
 	get_fd_safe_dev_ops(&ops);
 
 	struct ntfs_device* dev = ntfs_device_alloc("", 0, &ops, NULL);
 	if ((dev->d_private = malloc(sizeof(int))) == NULL)
-		return -1;
+		return -errno;
 	*(int*)(dev->d_private) = img;
 
 	ntfs_volume* vol = ntfs_device_mount(dev, NTFS_MNT_RDONLY);
@@ -53,13 +54,23 @@ int dump_file(int img, const char *path, int out)
 		return -errno;
 	}
 
+	// We need to have the self errno variable, because the NTFS API functions
+	// change errno to 0 if all good
+	int local_errno = 0;
+
 	ntfs_inode* file_inode = ntfs_pathname_to_inode(vol, NULL, path);
 	if (file_inode == NULL)
+	{
+		local_errno = errno;
 		goto umount;
+	}
 
 	ntfs_attr* na = ntfs_attr_open(file_inode, AT_DATA, AT_UNNAMED, 0);
 	if (na == NULL)
+	{
+		local_errno = errno;
 		goto close_inode;
+	}
 
 	u8 buf[NTFS_BUF_SIZE];
 	s64 size = na->data_size;
@@ -68,11 +79,17 @@ int dump_file(int img, const char *path, int out)
 	{
 		s64 size_read = ntfs_attr_pread(na, offset, NTFS_BUF_SIZE, buf);
 		if (size_read == 0)
+		{
+			local_errno = errno;
 			goto close_attr;
-		
+		}
+
 		s64 size_write = write(out, buf, size_read);
 		if (size_write != size_read)
+		{
+			local_errno = errno;
 			goto close_attr;
+		}
 
 		size -= size_read;
 		offset += size_read;
@@ -87,5 +104,5 @@ close_inode:
 umount:
 	ntfs_umount(vol, TRUE);
 
-	return -errno;
+	return -(local_errno ? local_errno: errno);
 }
