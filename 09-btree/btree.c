56f396db41b63a868adea7ff96a3bcea981d6158
DenRen <denrenruslan@gmail.com>
2022-11-30 01:51:09 +0300

    Start implement btree

--- 09-btree/btree.c
+++ 09-btree/btree.c
@@ -1,63 +1,60 @@
 #include <solution.h>
 #include <stdlib.h>
 
-struct btree
-{
-	/* implement me */
-};
+#include "btree.hpp"
+
+struct btree : public BTree<int>
+{};
 
 struct btree* btree_alloc(unsigned int L)
 {
-	(void) L;
-
-	return NULL;
+	return new btree{ 2 * L};
 }
 
 void btree_free(struct btree *t)
 {
-	(void) t;
+	delete t;
 }
 
 void btree_insert(struct btree *t, int x)
 {
-	(void) t;
-	(void) x;
+	t->insert(x);
 }
 
 void btree_delete(struct btree *t, int x)
 {
-	(void) t;
-	(void) x;
+	t->remove(x);
+	t->draw();
 }
 
 bool btree_contains(struct btree *t, int x)
 {
-	(void) t;
-	(void) x;
-
-	return false;
+	return t->contain(x).node != nullptr;
 }
 
-struct btree_iter
+struct btree_iter : BTree<int>::Iterator
 {
 };
 
 struct btree_iter* btree_iter_start(struct btree *t)
 {
-	(void) t;
-
-	return NULL;
+	return new btree_iter{ t->begin() };
 }
 
 void btree_iter_end(struct btree_iter *i)
 {
-	(void) i;
+	delete i;
 }
 
 bool btree_iter_next(struct btree_iter *i, int *x)
 {
-	(void) i;
-	(void) x;
+	auto& it = *i;
 
-	return false;
+	if (it.is_end())
+		return false;
+
+	*x = *it;
+	++it;
+
+	return true;
 }
